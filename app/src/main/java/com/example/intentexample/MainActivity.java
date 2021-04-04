1c2fb8a6d5253052c4eadca70733ea5ca562d067
DongMin-0604 <dongmin3147@gmail.com>
2021-04-04 12:03:33 +0900

    엑티비티 값 넘기기,ImageView & Toast 이미지 클릭시 팝업 메세지 출력

--- app/src/main/java/com/example/intentexample/MainActivity.java
+++ app/src/main/java/com/example/intentexample/MainActivity.java
@@ -7,12 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_move;
     private EditText et_test;
     private String str;
+    ImageView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this , SubActivity.class);
                 intent.putExtra("str",str);
                 startActivity(intent);
+            }
+        });
+        test = (ImageView)findViewById(R.id.test);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"팝업 메세지 입니다.",Toast.LENGTH_SHORT).show();
             }
         });
     }
