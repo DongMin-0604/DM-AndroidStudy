3f06a021158a5e4bb2c9d6948d1954610c375130
DongMin-0604 <dongmin3147@gmail.com>
2021-04-13 09:38:29 +0900

    ListView 첫번째

--- app/src/main/java/com/example/intentexample/MainActivity.java
+++ app/src/main/java/com/example/intentexample/MainActivity.java
@@ -2,45 +2,33 @@ package com.example.intentexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn_move;
-    private EditText et_test;
-    private String str;
-    ImageView test;
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et_test = findViewById(R.id.et_test);
+        list = (ListView)findViewById(R.id.list);
 
+        List<String> data = new ArrayList<>();
 
-        btn_move = findViewById(R.id.btn_move);
-        btn_move.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                str = et_test.getText().toString();
-                Intent intent = new Intent(MainActivity.this , SubActivity.class);
-                intent.putExtra("str",str);
-                startActivity(intent);
-            }
-        });
-        test = (ImageView)findViewById(R.id.test);
-        test.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"팝업 메세지 입니다.",Toast.LENGTH_SHORT).show();
-            }
-        });
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,data);
+        list.setAdapter(adapter);
+
+        data.add("리스트1");
+        data.add("리스트2");
+        data.add("리스트3");
+        adapter.notifyDataSetChanged();
+
     }
 }
